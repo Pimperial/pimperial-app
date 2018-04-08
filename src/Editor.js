09db96d599fbc6d19095fc02ded643e864370f24
Priansh Shah <shah@priansh.com>
2018-04-07 21:02:37 -0400

    save bio

--- src/Editor.js
+++ src/Editor.js
@@ -5,6 +5,9 @@ import { Screen, View, TextInput, Button, Text, Image, NavigationBar, Title, Ico
 let screen = Dimensions.get('window')
 
 export default class Entry extends React.Component {
+    state = {
+        bio: ''
+    }
     getAge(email) {
         let year_started = email.split('@')[0]
         year_started = year_started.substr(year_started.length - 2, 2)
@@ -26,7 +29,34 @@ export default class Entry extends React.Component {
                 <NavigationBar
                     centerComponent={<Title>EDIT PROFILE</Title>}
                     rightComponent={<Icon name="checkbox-on" onPress={() => {
-                        Alert.alert("Saving profile!")
+                        fetch(
+                            `http://54.91.147.183:3415/api/${this.props.user.code}/bio`,
+                            {
+                                method: 'post',
+                                headers: {
+                                    'Accept': 'application/json',
+                                    'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify({
+                                    description: this.state.bio
+                                })
+                            }
+                        ).then((s) => {
+                            if (s.status == 200) Alert.alert('Saved profile!', 'Your profile has been saved.')
+                            else Alert.alert(
+                                "It's not you, it's us! 😞",
+                                "We couldn't save your profile, please try again later."
+                            )
+                        }).catch((e) => {
+                            if (e) {
+                                console.debug(e)
+                                Alert.alert(
+                                    "It's not you, it's us! 😞",
+                                    "We couldn't save your profile, please try again later."
+                                )
+                            }
+                        })
+                        Alert.alert(this.state.bio)
                     }} />}
                 />
                 <View style={{
@@ -114,7 +144,10 @@ export default class Entry extends React.Component {
                             textAlignVertical: 'top',
                             marginTop: 20,
                             borderWidth: 0.2, borderColor: '#bbb'
-                        }} placeholder={"Tell us about yourself!"} />
+                        }} placeholder={"Tell us about yourself!"} value={this.props.user.bio}
+                            onChangeText={(t) => {
+                                this.setState({ bio: t })
+                            }} />
                         <Text style={{
                             alignSelf: 'center',
                             marginTop: 30
